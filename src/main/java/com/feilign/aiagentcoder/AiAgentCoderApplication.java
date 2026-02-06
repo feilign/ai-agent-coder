0bf071e6e15a85043a6e899fe4b06234f0af52ad
feilign <feilign@yeah.net>
2026-02-06 18:51:59 +0800

    Create Spring Boot main application class

---
+++ src/main/java/com/feilign/aiagentcoder/AiAgentCoderApplication.java
@@ -0,0 +1,11 @@
+package com.feilign.aiagentcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAgentCoderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AiAgentCoderApplication.class, args);
+    }
+}
